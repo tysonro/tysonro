993bd81864abcd7ce222b6ca430c3091fc34f7d3
Tyson O'Keefe <tokeefe@berrydunn.com>
2022-06-03 13:46:47 -0400

    Removing Set-DisRes as it wasn't working

--- OSDCloud-ZTI.ps1
+++ OSDCloud-ZTI.ps1
@@ -12,12 +12,6 @@ $OSBuild = '21H2'
 $OSEdition = 'Pro'
 $Interactive = $false
 
-if ((Get-MyComputerModel) -match 'VMware7,1') {
-    # Change Display Resolution for Virtual Machine
-    Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600   
-}
-
 Write-Host "=========================================================================" -ForegroundColor Cyan
 Write-Host "===================== Cloud Image Deployment Script =====================" -ForegroundColor Cyan
 Write-Host "=========================================================================" -ForegroundColor Cyan
---
+++ params.ps1
@@ -0,0 +1,18 @@
+
+
+# parameters to try for "Start-OSDCloud"
+
+# Checks microsoft catalog for firmware updates?
+# Possibly helpful for the old t15's? How is jamie doing it? This could be much simpler and automated... Test it out!
+-Firmware
+
+# Skip the autopilot task routine
+-SkipAutopilot
+
+# Skips the ODS task routine... not exactly sure what this is?
+-SkipODT
+
+# would this work better/simplify code to use this here instead of adding it into the zti script?
+-Restart
+
+
